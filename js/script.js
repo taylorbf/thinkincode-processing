cb0716a46aa460878005b82b1eccab1677d3ae4a
Ben Taylor <taylorbf@gmail.com>
2015-06-24 17:45:05 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+/*	$(".example").mouseover(function() {
+		$(this).find(".hint").show()
+	})
+
+	$(".example").mouseout(function() {
+		$(this).find(".hint").hide();
+	}) */
+
+})
